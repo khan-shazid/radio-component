4c55ff3714092683cd3e96220a425c1aae9a2120
khan-shazid <shazid1971@gmail.com>
2021-12-18 22:42:59 +0600

    use state testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Station from './components/Station/station';
 import { SAMPLE_DATA } from './constants';
 
+import UseStateTutorial from './usestate-tutorial';
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <Station stations={SAMPLE_DATA}/>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
-        <Station stations={SAMPLE_DATA}/>
+        <UseStateTutorial/>
     </div>
   );
 }
